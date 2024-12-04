5fd218b76849f8804acb6b909c5fa7d3ee55eb89
Molly Mlynarczyk <mlynarczyk.molly@principal.com>
2024-12-04 08:55:45 -0800

    Day 2 part 2 not done

--- day2/day2.py
+++ day2/day2.py
@@ -3,22 +3,24 @@
 
 import sys
 
+# returns unsafe index or -1 if all are safe
 def is_safe(report):
     previous_diff = 0
     for i in range(len(report) - 1):
         diff = int(report[i]) - int(report[i + 1])
+        # print(i)
         # print(diff)
-        # print("previous: " + str(previous_diff) + " current: " + str(diff))
+        #print("previous: " + str(previous_diff) + " current: " + str(diff))
         if previous_diff < 0 and diff > 0:
-            return False
+            return i + 1 
         elif previous_diff > 0 and diff < 0:
-            return False
+            return i
         elif diff == 0:
-            return False
+            return i
         elif abs(diff) > 3:
-            return False
+            return i + 1
         previous_diff = diff
-    return True
+    return -1
 
 f = open(sys.argv[1], "r")
 
@@ -27,11 +29,23 @@ safe = 0
 reports = f.readlines()
 
 for report in reports:
-    # print(report)
     report.strip()
+    original_report = report
     report = report.split()
-    if is_safe(report):
+    unsafe = is_safe(report)
+    if unsafe == -1:
         safe += 1
+    else:
+        # remove bad index & rerun
+        popped = report.pop(unsafe)
+        # print(popped)
+        # print(unsafe)
+        unsafe = is_safe(report)
+        if unsafe == -1:
+            safe += 1
+        else:
+            print(original_report)
+
 print(safe)
 # similarity_score = 0
 # for i in range(len(right_list)):
